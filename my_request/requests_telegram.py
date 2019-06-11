e52c04a7cd26687b1247be2d79730d1cfabce4f7
carlos antonio <carlosaarivera23@gmail.com>
2019-06-11 01:51:51 -0500

    Se añade codigo para hacer un boto que repite los mensajes que recibe

--- my_request/requests_telegram.py
+++ my_request/requests_telegram.py
@@ -2,10 +2,10 @@
 # -*- encoding:utf-8 -*-
 
 import os
+import queue
 import time
 
 import requests
-import json
 import logging
 import threading
 
@@ -14,15 +14,17 @@ logger = logging.getLogger('my_requests')
 token = os.getenv('token')
 base_url = 'https://api.telegram.org/bot{}/{}'
 
+queue_message = queue.Queue()
 
-def send_message(chat_id, message, token, parse_mode='HTML'):
+
+def send_message(chat_id, message, token_bot, parse_mode='HTML'):
     """
     Envia un mensaje al chat espesificado a travez de la api de telegram bot
 
     Args:
         chat_id(int, str): id que identifica al chat.
         message(str): Mensaje ue se quiere enviar.
-        token(str): token espesifico del bot que se quiere utilizar
+        token_bot(str): token espesifico del bot que se quiere utilizar
         parse_mode(str): Tipo de parser que se utilizara para enviar el mensaje.
 
     Returns:
@@ -30,7 +32,7 @@ def send_message(chat_id, message, token, parse_mode='HTML'):
     """
     params = {'chat_id': str(chat_id), 'text': message, 'parse_mode': parse_mode}
     try:
-        response = requests.get(url=base_url.format(token, 'sendMessage'), params=params).json()
+        response = requests.get(url=base_url.format(token_bot, 'sendMessage'), params=params).json()
     except Exception as details:
         logger.warning('Error al intentar enviar el mensaje.\n'
                        'Details: {}'.format(details))
@@ -50,13 +52,13 @@ def request_is_ok(response):
     Returns:
         None
     """
-    if response.status_code == requests.codes.ok:
+    if response['ok']:
         logger.debug('La solisutd se ejecuto correctamente.')
         return True
     else:
         logger.warning('Algun error ocurrio en la solicitud.\n'
-                       'Error: {}'.format(response.status_code))
-        raise MethodRequestError('Codigo de error: {}'.format(response.status_code))
+                       'Error: {}'.format(response))
+        raise MethodRequestError('Codigo de error: {}'.format(response))
 
 
 class Updates(threading.Thread):
@@ -74,13 +76,18 @@ class Updates(threading.Thread):
             datas = {}
             if update:
                 datas = {e['update_id']: e for e in update['result']}
-                last_data_id = max(datas)
+                try:
+                    last_data_id = max(datas)
+                except ValueError:
+                    last_data_id = -1
             else:
                 self.__logger.warning('SIn datos')
                 last_data_id = -1
             if last_data_id > self.last_data:
                 self.last_data = last_data_id
-                print(datas[last_data_id])
+                queue_message.put(datas[last_data_id])
+                send_message(datas[last_data_id]['message']['chat']['id'], datas[last_data_id]['message']['text'],
+                             token)
 
     def stop(self):
         if not self.__stop.is_set():
@@ -90,6 +97,24 @@ class Updates(threading.Thread):
 
     def get_updates(self):
         return requests.get(base_url.format(self.__token, 'getUpdates')).json()
+
+
+class RepliMessage(threading.Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
+        self.__logger = logging.getLogger(name)
+        self.__stop = threading.Event()
+
+    def run(self) -> None:
+        while not self.__stop.is_set():
+            if not queue_message.empty():
+                message = queue_message.get()
+
+    def stop(self):
+        if not self.__stop.is_set():
+            self.__stop.set()
+        else:
+            self.__logger.warning('El hilo fue detenido previamente')
 
 
 class MethodRequestError(Exception):
@@ -106,3 +131,8 @@ while hilo.is_alive():
         time.sleep(.1)
     except KeyboardInterrupt:
         hilo.stop()
+
+# TODO hacer una base de datos para almacenar la informacion.
+# TODO ALmacenar ultimo id de actualizacion.
+# TODO ALmacenar la hoay fecha de la ultima actualizacion.
+# TODO Crear un usuario cuando se inicie un nuevo chat.
